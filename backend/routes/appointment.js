e1dad501f48b23406b86a9c993abb0a1fea19232
Sandeep Rudola <sandeeprudola10@gmail.com>
2025-09-03 22:20:49 +0530

    appointment creation endpoint

---
+++ backend/routes/appointment.js
@@ -0,0 +1,68 @@
+const express=require('express')
+const router = express.Router();
+const Appointment=require('../models/Appointment')
+const auth=require('../middlewares/authmiddleware')
+const User=require('../models/User')
+const Emp=require('../models/Emp')
+const zod=requrie('zod')
+
+const createAppointmentSchema = zod.object({
+    staff: zod.string(),
+    appointmentdate: zod.string(),
+    appointmentType: zod.enum(['consultation','speech-therapy','hearing-test','followup','emergency']),
+    notes: zod.string().optional()
+});
+const updateAppointmentSchema = zod.object({
+    duration:zod.number().optional(),
+    status:zod.enum(['scheduled','confirmed','in-progress','completed','canceled']),
+    priority:zod.enum(['low','normal','high','emergency']),
+    appointmentdate: zod.string(),
+    notes: zod.string().optional(),
+    appointmentType: zod.enum(['consultation','speech-therapy','hearing-test','followup','emergency']),
+    paymentStatus: zod.enum(['pending','paid','partial','waived']).optional()
+});
+
+router.post("/user",auth,async(req,res)=>{
+    try{
+        const parsed=createAppointmentSchema.safeparse(req.body);
+        if(!parsed.success){
+            return res.status(400).json({
+                msg:"invalid data"
+            })
+        }
+
+        const data=parsed.data;
+        const staff=await Emp.findById(data.staff);
+        if(!staff){
+            return res.status(404).json({msg:"no staff exist"})
+        }
+        if(!staff.isActive){
+            return res.status(400).json({msg:"no staff is not active right now"})
+        }
+
+        const appointment=new Appointment({
+            patient:req.user.id,
+            staff:data.staff,
+            appointmentdate:data.appointmentdate,
+            duration:data.duration??30,
+            appointmentType:data.appointmentType,
+            notes:data.notes,
+        })
+
+        await appointment.save()
+        await appointment.populate('patient staff','firstName lastName email specialization role')
+        res.status(201).json({
+            msg:"appointment created successfully",
+            appointment
+        })
+    }
+    catch(err){
+        return res.status(500).json({
+            msg:"failed to create appointment"
+        })
+    }
+    
+
+})
+
+module.exports=router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const express=require('express')
 const userRouter=require("./user")
 const employeeRouter=require("./employee")
 const adminRouter=require("./admin");
+const Appointment = require('../models/Appointment');
 
 const router=express.Router();
 
 router.use("/user",userRouter)
 router.use("/admin",adminRouter)
 router.use("/employee",employeeRouter)
+router.use("/appointment",Appointment)
 
 module.exports=router;
