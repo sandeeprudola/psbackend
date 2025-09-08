8a6886dff19565bca7d8fa65ce4f036517219e7c
Sandeep Rudola <sandeeprudola10@gmail.com>
2025-09-08 22:28:34 +0530

    admin route started

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,9 +1,19 @@
 const express=require('express')
 const router=express.Router();
 const auth=require("../middlewares/adminAuth")
+const zod=require('zod')
 
+const adminSchema=zod.object({
+    username:zod.string(),
+    password:zod.string(),
+    firstName:zod.string(),
+    lastName:zod.string(),
+    email:zod.string(),
+    role:zod.string(),
+    caninvite:zod.string()
+})
 
-
+router.post("/signup",)
 
 
 module.exports=router;
