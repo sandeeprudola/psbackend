a2661bd69075888aaa61ecc692e245a7441bbfd6
Sandeep Rudola <sandeeprudola10@gmail.com>
2025-09-08 22:20:06 +0530

    admin middleware created

---
+++ backend/middlewares/adminAuth.js
@@ -0,0 +1,36 @@
+const {JWT_SECRET}=require("../config")
+const jwt=require('jsonwebtoken');
+const Admin = require("../models/Admin");
+
+const adminAuth = async (req, res, next) => {
+    const authHeader=req.headers.authorization
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(411).json({
+            msg:"invalid credentials"
+        })
+    }
+
+    try{
+    const token=authHeader.split(" ")[1];
+    const decoded=jwt.verify(token,JWT_SECRET)
+
+    const admin=await Admin.findById(decoded.id)
+    if(!admin){
+        return res.status(401).json({
+            msg:"no admin found"
+        })
+    }
+    req.admin=admin;
+    next();
+    }
+
+    catch(err){
+        return res.status(411).json({
+            msg:"there is some problem in your try code"
+        })
+    }
+  };
+
+  module.exports={
+    adminAuth
+  }
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,4 +1,9 @@
 const express=require('express')
 const router=express.Router();
+const auth=require("../middlewares/adminAuth")
+
+
+
+
 
 module.exports=router;
